7d12f5324c07e841a5937039d60dc7a5f21a571e
Norsang <thenorsang@gmail.com>
2019-02-21 20:48:40 -0500

    added home make server view

--- server.js
+++ server.js
@@ -3,7 +3,24 @@ var express= require("express")
 var path = require("path")
 
 var app = express();
-var port = 8080;
+var PORT = 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+var table = [
+    {
+      id: "22",
+      name: "Customer 1",
+      email: "customer@gmail.com",
+      phone: 1112223333
+    }
+  ];
+
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "home.html"));
+  });
+
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
